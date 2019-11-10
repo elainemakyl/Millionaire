db5d9f32beb9e8fe75302cd61838151e87975722
minimikan <minimikan@outlook.com>
2019-11-10 14:18:46 +0800

    Add 2 swift

---
+++ Millionare/IncomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  IncomeViewController.swift
+//  Millionare
+//
+//  Created by mat on 10/11/2019.
+//  Copyright © 2019 EE4304. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import UIKit
+
+class IncomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ SpeningViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SpeningViewController.swift
+//  Millionare
+//
+//  Created by mat on 10/11/2019.
+//  Copyright © 2019 EE4304. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import UIKit
+
+class SpeningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
