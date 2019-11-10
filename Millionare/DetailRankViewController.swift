0b09010e86327916367c787b8eb99e546759595b
leo <pk999pk77@gmail.com>
2019-11-10 14:57:18 +0800

    leo v.1

---
+++ Millionare/DetailRankViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailRankViewController.swift
+//  Millionare
+//
+//  Created by Wing Kei Ma on 10/11/2019.
+//  Copyright © 2019 EE4304. All rights reserved.
+//
+
+import UIKit
+
+class DetailRankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
