a3873585660d39df2fdd239b3ec10b43b5e064b7
leo <pk999pk77@gmail.com>
2019-11-11 00:01:36 +0800

    leo

--- Millionare/RankViewController.swift
+++ Millionare/RankViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class RankViewController: UIViewController {
+class RankViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let items:[String] = ["123","456","789"]
+    @IBOutlet var table: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
