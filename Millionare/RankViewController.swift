fbc0b779c4c52a69e38d784388ad91a617462251
leo <pk999pk77@gmail.com>
2019-11-13 00:57:32 +0800

    leo update search bar

--- Millionare/RankViewController.swift
+++ Millionare/RankViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RankViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class RankViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     let items:[String] = ["test","456","ac","999"]
     let email:[String] = ["hi@gmail.com", "you@gmail.com", "test@gmail.com", "ac@gmail.com"]
@@ -16,9 +16,9 @@ class RankViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet var table: UITableView!
     
 
+    @IBOutlet var searchBar: UISearchBar!
     
-    
-    
+    var filterData: [String]!
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -27,14 +27,14 @@ class RankViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return filterData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RankTableViewCell
    //  cell.textLabel?.text = items[indexPath.row]
         
-        cell.nameLabel.text = items[indexPath.row]
+        cell.nameLabel.text = filterData[indexPath.row]
         cell.emailLabel.text = email[indexPath.row]
         cell.rankLabel.text = "Rank " + String(indexPath.row + 1)
         return cell
@@ -49,7 +49,26 @@ class RankViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     
     }
-
+    
+    
+    // Update filterdata when user type in searchbar
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterData = searchText.isEmpty ? items : items.filter({(dataString: String) -> Bool in return dataString.range(of: searchText, options: .caseInsensitive) != nil })
+        //
+        table.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         
         table.reloadData()
@@ -59,6 +78,9 @@ class RankViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        searchBar.delegate = self
+        filterData = items
     }
 
     
