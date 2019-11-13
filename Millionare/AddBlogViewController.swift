4238591674a4a420c01c7a91136af472924414c8
Michael Chan <michaelchan@Michaels-Mac.local>
2019-11-13 03:32:08 -0800

    blog layout

---
+++ Millionare/AddBlogViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddBlogViewController.swift
+//  Millionare
+//
+//  Created by Michael Chan on 13/11/2019.
+//  Copyright © 2019 EE4304. All rights reserved.
+//
+
+import UIKit
+
+class AddBlogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
